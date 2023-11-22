9749a5f52ff24e93311d7081c3cdafabe0a9fbae
Vishal-Kaushik-Git <149059617+Vishal-Kaushik-Git@users.noreply.github.com>
2023-11-22 21:32:54 +0530

    saving your mistake mr.git

--- logical questions/bracket.java
+++ logical questions/bracket.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
 import java.util.Stack;
 
 public class bracket {
     public static void main(String[] args){
-        Stack<Integer> stc = new Stack<>();
-        for(int i =0; i<stc.lastElement(); i++){
-            if(){
-
+        Stack<Character> stc = new Stack<>();
+        String str = new String();
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the string ");
+        str = sc.nextLine();
+        for(int i =0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == '('){
+                stc.push(i);
             }
         }
     }
