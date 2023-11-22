232b7003e719ba62a6fbcd0aefad063d78eccbe2
Vishal-Kaushik-Git <149059617+Vishal-Kaushik-Git@users.noreply.github.com>
2023-11-22 20:54:56 +0530

    Stack added

--- logical questions/MergeString.java
+++ logical questions/MergeString.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class MergeString {
+
+    public static void Merge(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < (str1.length() + str2.length()); i++) {
+            if (i < str1.length()) {
+                sb.append(str1.charAt(i));
+            }
+            if (i < str2.length()) {
+                sb.append(str2.charAt(i));
+            }
+        }
+        System.out.println("Merge string " + sb);
+
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first string ");
+        String str1 = sc.nextLine();
+        System.out.println("Enter second string ");
+        String str2 = sc.nextLine();
+        Merge(str1, str2);
+        sc.close();
     }
 }
--- logical questions/bracket.java
+++ logical questions/bracket.java
@@ -1,3 +1,12 @@
+import java.util.Stack;
+
 public class bracket {
-    
+    public static void main(String[] args){
+        Stack<Integer> stc = new Stack<>();
+        for(int i =0; i<stc.lastElement(); i++){
+            if(){
+
+            }
+        }
+    }
 }
