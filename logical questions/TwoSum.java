2a4125bd17ea95a979bf507c14b557db15932e8d
Vishal-Kaushik-Git <vishal2wishall@gmail.com>
2023-11-22 13:21:57 +0530

    Added new Files into logical question

--- logical questions/TwoSum.java
+++ logical questions/TwoSum.java
@@ -1,15 +1,16 @@
 public class TwoSum {
     public static void main(String args[]){
         int[] nums = {1,3,5,6};
-        int target = 6;
+        int target = 10;
 
         for(int i=0; i<nums.length; i++){
-           for(int j=1; j<5; j++){
+           for(int j=i+1; j<nums.length; j++){
             if(nums[i] + nums[j] == target){
                 System.out.println(i+","+j);
+            }
             }
            }
         }
 
     }    
-}
+
---
+++ logical questions/longestCommonPrefix.java
@@ -0,0 +1,9 @@
+public class longestCommonPrefix {
+    public static void main(String[] args) {
+    String[] strArray = {"Fix", "Fish", "Finish"};
+    StringBuilder sb = new StringBuilder();
+     for(int i =0; i<strArray.length; i++){
+        System.out.println(strArray[i]);
+     }
+    }
+}
