ea54ff4c27d93a2602dfa884f284d0bc3d74daf9
Vishal-Kaushik-Git <vishal2wishall@gmail.com>
2023-11-23 13:57:44 +0530

    Bracket

--- logical questions/bracket.java
+++ logical questions/bracket.java
@@ -2,17 +2,44 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class bracket {
-    public static void main(String[] args){
-        Stack<Character> stc = new Stack<>();
-        String str = new String();
-        Scanner sc  = new Scanner(System.in);
-        System.out.println("Enter the string ");
-        str = sc.nextLine();
+
+    public static boolean isvalid(String str){
+     Stack<Character> stc = new Stack<>();
         for(int i =0; i<str.length(); i++){
             char ch = str.charAt(i);
             if(ch == '('){
-                stc.push(i);
+                stc.push(ch);
+            }else if(ch == '{'){
+                stc.push(ch);
+            }else if(ch == '['){
+                stc.push(ch);
+            }else{
+            if(stc.size()==0){
+                return false;
+            }
+            if(stc.peek() == '('){
+                stc.pop();
+
+            }else if(stc.peek() == '{'){
+                stc.pop();
+              
+            }else if(stc.peek() == '['){
+                    stc.pop();
+                }
             }
         }
+        if(stc.size()>0){
+            return false;
+        }
+        return true;
+        
+    }
+
+    public static void main(String[] args){
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the string ");
+        String str = sc.nextLine();
+        System.out.println(isvalid(str));
+        sc.close();
     }
 }
