302db5afac87e9aa1969bed2d4316306bd4a7fc8
Vishal-Kaushik-Git <vishal2wishall@gmail.com>
2023-12-02 21:03:41 +0530

    Update basicgui.java

--- Gui/basicgui.java
+++ Gui/basicgui.java
@@ -1,20 +1,31 @@
+import java.awt.Color;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class basicgui {
     public static void main(String[] args) {
         JLabel label = new JLabel();
-        JFrame frame= new JFrame();
-        frame.setSize(300, 300);
+        JFrame frame= new  JFrame();
+        JPanel panel = new JPanel();
+
+        frame.setSize(600, 600);
         frame.setVisible(true);
         frame.setTitle("Basic Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        ImageIcon IG = new ImageIcon("insta.webp");
+
+        ImageIcon IG = new ImageIcon("Gui\\Images\\Emoji.png");
         frame.setIconImage(IG.getImage());
+
         label.setText("Hello World");
         frame.add(label);
-        frame.pack();
+
+        panel.setBackground(Color.BLUE);
+        panel.setBounds(0 , 0, 300, 300);
+        frame.add(panel);
+        
     }
 }
